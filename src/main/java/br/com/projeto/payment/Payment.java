14207c36374805b4151aa09408cca082b07489b6
Jefferson Lisboa de Souza <lisboa.jeff@gmail.com>
2020-02-07 08:17:14 -0300

    better flow logic was inserted in the payment policy

--- src/main/java/br/com/projeto/payment/Payment.java
+++ src/main/java/br/com/projeto/payment/Payment.java
@@ -2,8 +2,8 @@ package br.com.projeto.payment;
 
 import java.math.BigDecimal;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * This class should be stateless
@@ -15,20 +15,9 @@ final class Payment implements Payments {
     @Override
     public Map<Integer, BigDecimal> constructPaymentMapByClient(final Collection<PaymentAdapter> paymentAdapters) {
 
-        final HashMap<Integer, BigDecimal> map = new HashMap<>();
-
-        paymentAdapters.forEach(paymentAdapter -> construct(map, paymentAdapter));
-
-        return map;
-
-    }
-
-    private void construct(final HashMap<Integer, BigDecimal> map, final PaymentAdapter paymentAdapter) {
-
-        final Integer clientId = paymentAdapter.getClientId();
-
-        map.put(clientId, map.getOrDefault(clientId, BigDecimal.ZERO).add(paymentAdapter.getValue()));
-
+        return paymentAdapters
+                .stream()
+                .collect(Collectors.toMap(PaymentAdapter::getClientId, PaymentAdapter::getValue, BigDecimal::add));
     }
 
 }
